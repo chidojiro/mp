33096298fbd3090ed1b98b7df7b9920f0d2a06b2
Nick <chidojiro@gmail.com>
2022-02-17 09:35:28 +0700

    fix: iamge warnings

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image';
-
 export const Header = () => {
   return (
     <div className='flex items-center justify-between h-12 px-10 border-b border-gray-300 '>
       <div className='flex items-center h-full'>
-        <img src='images/GMP_givery.png' className='w-25 h-7' />
-        <img src='images/mp.png' className='h-7' />
+        <img src='/images/GMP_givery.png' alt='' className='w-25 h-7' />
+        <img src='/images/mp.png' alt='' className='h-7' />
       </div>
       <div className='font-semibold text-medium'>demo@gmail.com</div>
     </div>
