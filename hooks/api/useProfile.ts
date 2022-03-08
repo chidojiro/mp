3a0979361f9d1e0e106a78393c832a577c10ff1f
Nick <chidojiro@gmail.com>
2022-03-08 11:08:52 +0700

    chore: useProfile

--- hooks/api/useProfile.ts
+++ hooks/api/useProfile.ts
@@ -2,27 +2,14 @@ import { useServerSidePropsContext } from '@/contexts';
 import { ProfileApis } from '@/apis';
 import useSWR from 'swr';
 import { Profile } from '@/types';
-
-const defaultValue: Profile = {
-  created_at: '',
-  email: '',
-  last_name: '',
-  first_name: '',
-  updated_at: '',
-};
+import React from 'react';
 
 export function useProfile() {
   const { props } = useServerSidePropsContext();
-  const { data, mutate, error } = useSWR<Profile | null>('/user/me', ProfileApis.get, {
+
+  const swrReturn = useSWR<Profile>('/user/me', ProfileApis.get, {
     fallbackData: props.profile,
   });
 
-  const loading = !data && !error;
-
-  return {
-    loading,
-    mutate,
-    data: data ?? props.profile ?? defaultValue,
-    error,
-  };
+  return React.useMemo(() => ({ ...swrReturn, data: swrReturn.data! }), [swrReturn]);
 }
