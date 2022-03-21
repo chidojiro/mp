99e1db527f2c19e8a93826cde3df50d7b25be5fa
tinh <nttinh995@gmail.com>
2022-03-21 09:34:37 +0700

    fix scroll to next step when hit button

--- components/marketingAction/MyMarketingAction/Edit/Steps.tsx
+++ components/marketingAction/MyMarketingAction/Edit/Steps.tsx
@@ -11,6 +11,20 @@ type Props = {
 export const Steps = ({ steps, onConfirm, onShowPreview }: Props) => {
   const idLastStep = steps.length - 1;
 
+  const refs = steps.reduce((acc: any, step: Step) => {
+    acc[step.id] = React.createRef();
+    return acc;
+  }, {});
+
+  const handleConfirm = (stepId: number) => {
+    onConfirm(stepId);
+    if (stepId !== idLastStep) {
+      refs[stepId + 1]?.current.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <>
       {steps.map((step: Step, idx: number) => (
@@ -18,8 +32,9 @@ export const Steps = ({ steps, onConfirm, onShowPreview }: Props) => {
           step={step}
           isLastStep={idx !== idLastStep}
           key={step.id}
-          onConfirm={onConfirm}
+          onConfirm={handleConfirm}
           onShowPreview={onShowPreview}
+          ref={refs[step.id]}
         />
       ))}
     </>
