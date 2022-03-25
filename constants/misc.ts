7638f5076d5958b5413deb9fca2df246a9e292a6
Nick <chidojiro@gmail.com>
2022-03-25 10:44:33 +0700

    fix: correct class name mention item

--- constants/misc.ts
+++ constants/misc.ts
@@ -1,2 +1,2 @@
-export const MentionValuePattern = /<span class="mention-item--inner"[^\/]*<\/span>/g;
+export const MentionValuePattern = /<span class="mention-item"[^\/]*<\/span>/g;
 export const ZeroWidthWhitespace = '&#8203;';
--- utils/mention.ts
+++ utils/mention.ts
@@ -7,7 +7,7 @@ const insert = ({ value, label }: Option<string>) => {
     false,
     `
     <span class='mention-item--left-zww'>&#8203</span>
-    <span class='mention-item--inner' contenteditable=false data-value=${value}>${label}</span>
+    <span class='mention-item' contenteditable=false data-value=${value}>${label}</span>
     <span class='mention-item--right-zww'>&#8203</span>
     `
   );
