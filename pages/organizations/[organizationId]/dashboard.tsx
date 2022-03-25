ebd4a7cc43b2486ad7157060b7650ad4667108ba
Michael <vuanhduc2685@gmail>
2022-03-25 16:12:52 +0700

    update(dashboard): try 2 clean things

--- pages/organizations/[organizationId]/dashboard.tsx
+++ pages/organizations/[organizationId]/dashboard.tsx
@@ -23,58 +23,56 @@ function Dashboard(props: any) {
   const data = mockData;
 
   return (
-    <ServerSidePropsProvider props={props}>
-      <Layout title={t('menuDashboard')}>
-        <h4 className='mt-5 mb-2 text-gray-600'>{t('dashboardCustomerAnalysis')}</h4>
+    <Layout title={t('menuDashboard')}>
+      <h4 className='mt-5 mb-2 text-gray-600'>{t('dashboardCustomerAnalysis')}</h4>
 
-        <div className='flex items-end justify-between w-full mb-10'>
-          <div className='flex'>
-            <div className='mr-5 font-bold'>{t('timePeriod')}</div>
-            <div>{t('dashboardPeriodLast180Days')}</div>
-          </div>
-          <CSVButton />
+      <div className='flex items-end justify-between w-full mb-10'>
+        <div className='flex'>
+          <div className='mr-5 font-bold'>{t('timePeriod')}</div>
+          <div>{t('dashboardPeriodLast180Days')}</div>
         </div>
+        <CSVButton />
+      </div>
 
-        <div className='w-full mb-12'>
-          {!!data && <CustomerSegmentTable data={data} onClick={() => console.log('click')} />}
+      <div className='w-full mb-12'>
+        {!!data && <CustomerSegmentTable data={data} onClick={() => console.log('click')} />}
+      </div>
+
+      <h4 className='mb-5 text-gray-600'>{t('labelReport')}</h4>
+
+      <div className='w-full'>
+        <div className='grid grid-cols-2 grid-rows-4 gap-[10px]'>
+          <CustomerReportButton
+            featuredIcon={<Icon name='f1-graduate' className='h-[25px] w-[65px]' />}
+            label={t('f2ConversionRate')}
+          />
+          <CustomerReportButton
+            featuredIcon={<Icon name='f2-graduate' className='h-[25px] w-[65px]' />}
+            label={t('semiLoyalConversionRate')}
+          />
+          <CustomerReportButton
+            featuredIcon={<Icon name='semi-loyal-graduate' className='h-[25px] w-[65px]' />}
+            label={t('loyalConversionRate')}
+          />
+          <CustomerReportButton
+            featuredIcon={<Icon name='loyal-fluctuate' className='h-[25px] w-[65px]' />}
+            label={t('loyalCustomerTrend')}
+          />
+          <CustomerReportButton
+            featuredIcon={<Icon name='sleep-returned' className='h-[25px] w-[65px]' />}
+            label={t('dormantCustomerReturnTrend')}
+          />
+          <CustomerReportButton
+            featuredIcon={<Icon name='f1-sleep-returned' className='h-[25px] w-[65px]' />}
+            label={t('dormantF1CustomerReturnTrend')}
+          />
+          <CustomerReportButton
+            featuredIcon={<Icon name='loyal-sleep-returned' className='h-[25px] w-[65px]' />}
+            label={t('dormantLoyalCustomerReturnTrend')}
+          />
         </div>
-
-        <h4 className='mb-5 text-gray-600'>{t('labelReport')}</h4>
-
-        <div className='w-full'>
-          <div className='grid grid-cols-2 grid-rows-4 gap-[10px]'>
-            <CustomerReportButton
-              featuredIcon={<Icon name='f1-graduate' className='h-[25px] w-[65px]' />}
-              label={t('f2ConversionRate')}
-            />
-            <CustomerReportButton
-              featuredIcon={<Icon name='f2-graduate' className='h-[25px] w-[65px]' />}
-              label={t('semiLoyalConversionRate')}
-            />
-            <CustomerReportButton
-              featuredIcon={<Icon name='semi-loyal-graduate' className='h-[25px] w-[65px]' />}
-              label={t('loyalConversionRate')}
-            />
-            <CustomerReportButton
-              featuredIcon={<Icon name='loyal-fluctuate' className='h-[25px] w-[65px]' />}
-              label={t('loyalCustomerTrend')}
-            />
-            <CustomerReportButton
-              featuredIcon={<Icon name='sleep-returned' className='h-[25px] w-[65px]' />}
-              label={t('dormantCustomerReturnTrend')}
-            />
-            <CustomerReportButton
-              featuredIcon={<Icon name='f1-sleep-returned' className='h-[25px] w-[65px]' />}
-              label={t('dormantF1CustomerReturnTrend')}
-            />
-            <CustomerReportButton
-              featuredIcon={<Icon name='loyal-sleep-returned' className='h-[25px] w-[65px]' />}
-              label={t('dormantLoyalCustomerReturnTrend')}
-            />
-          </div>
-        </div>
-      </Layout>
-    </ServerSidePropsProvider>
+      </div>
+    </Layout>
   );
 }
 
