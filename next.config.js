1d76e27c429726ebdb30a3eddf06ea02bb48fe3e
Nick <chidojiro@gmail.com>
2022-03-28 17:30:29 +0700

    fix: treeshake barrel file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { i18n } = require('./next-i18next.config');
-
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   i18n: {
     ...i18n,
     localeDetection: false,
   },
-  webpack(config) {
+  webpack(config, { isServer }) {
     config.module.rules.push({
       test: /\.svg$/,
       issuer: { and: [/\.(js|ts)x?$/] },
@@ -32,6 +34,13 @@ module.exports = {
       ],
     });
 
+    if (!isServer) {
+      config.module.rules.push({
+        test: /.+\/index\.ts/i,
+        sideEffects: false,
+      });
+    }
+
     return config;
   },
-};
+});
