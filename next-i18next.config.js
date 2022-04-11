9ef581cd5f85e3de90fc454e4add3238be5a64a2
Vu Anh Duc <v.duc@resola.ai>
2022-04-11 04:09:19 +0000

    fix[i18n]: reload messages when prerender

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja', 'en'],
     localeDetection: false,
-    reloadOnPrerender: false,
+    reloadOnPrerender: true,
     localePath: path.resolve('./public/locales'),
   },
 };
