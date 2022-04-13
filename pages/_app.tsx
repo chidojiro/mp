d10cfe577aa741859a80efb3f0586695d6f9d909
Nick <chidojiro@gmail.com>
2022-04-13 09:09:26 +0700

    fix: remove redirect login in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,50 +5,8 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { SWRConfig } from 'swr';
 import '../styles/globals.css';
-import { useRouter } from 'next/router';
-
-import { useAuth } from '@/hooks/useAuth';
 
 function CustomApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const auth = useAuth();
-  const [authorized, setAuthorized] = React.useState(false);
-  React.useEffect(() => {
-    function authCheck(url: string) {
-      // redirect to login page if accessing a private page and not logged in
-      const publicPaths = ['/login'];
-      const path = url.split('?')[0];
-      if (!publicPaths.includes(path)) {
-        if (auth.isAuthenticated) {
-          setAuthorized(true);
-        } else {
-          setAuthorized(false);
-          router.push({
-            pathname: '/login',
-          });
-        }
-      } else {
-        setAuthorized(true);
-      }
-    }
-
-    // run auth check on initial load
-    authCheck(router.asPath);
-
-    // set authorized to false to hide page content while changing routes
-    const hideContent = () => setAuthorized(false);
-    router.events.on('routeChangeStart', hideContent);
-
-    // run auth check on route change
-    router.events.on('routeChangeComplete', authCheck);
-
-    // unsubscribe from events in useEffect return function
-    return () => {
-      router.events.off('routeChangeStart', hideContent);
-      router.events.off('routeChangeComplete', authCheck);
-    };
-  }, [router]);
-
   return (
     <>
       <Head>
@@ -56,7 +14,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
       </Head>
       <div>
         <SWRConfig value={{ revalidateOnFocus: false }}>
-          {authorized ? <Component {...pageProps} /> : null}
+          <Component {...pageProps} />
         </SWRConfig>
       </div>
     </>
