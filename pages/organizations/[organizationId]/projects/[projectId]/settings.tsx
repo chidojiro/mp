583dff80f8fc1cd6baedf6881f6eedf6e0e0bfa7
Nick <chidojiro@gmail.com>
2022-04-15 17:33:32 +0700

    WIP

--- pages/organizations/[organizationId]/projects/[projectId]/settings.tsx
+++ pages/organizations/[organizationId]/projects/[projectId]/settings.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import { Settings } from '@/components/settings/Settings';
+import { Settings } from '@/components/settings';
 import { Layout } from '@/components/Layout';
 
 export const getServerSideProps = async ({ locale }: any) => ({
